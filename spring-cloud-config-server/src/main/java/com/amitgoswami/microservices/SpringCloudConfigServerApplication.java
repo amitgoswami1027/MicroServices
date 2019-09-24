ae273631e64484a6576e0ef6d2397adc061d1e51
Amit Goswami <amitgoswami1027@gmail.com>
2019-09-24 18:55:26 +0530

    Spring-cloud-config-server

--- spring-cloud-config-server/src/main/java/com/amitgoswami/microservices/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/amitgoswami/microservices/SpringCloudConfigServerApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+// This service helps to have the centralized store of all the services properties
+//http://localhost:8888/limits-service/default
+//http://localhost:8888/limits-service/dev
+//http://localhost:8888/limits-service/qa
+
 @EnableConfigServer
 @SpringBootApplication
 public class SpringCloudConfigServerApplication
