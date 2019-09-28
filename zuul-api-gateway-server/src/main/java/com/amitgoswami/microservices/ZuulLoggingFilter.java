83dbf6ba18c092c6ca9dce7519daa31d48a05835
Amit Goswami <amitgoswami1027@gmail.com>
2019-09-28 17:54:46 +0530

    Zuul api gateway

---
+++ zuul-api-gateway-server/src/main/java/com/amitgoswami/microservices/ZuulLoggingFilter.java
@@ -0,0 +1,47 @@
+package com.amitgoswami.microservices;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+
+@Component
+
+public class ZuulLoggingFilter extends ZuulFilter
+{
+    private Logger logger = (Logger) LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public String filterType()
+    {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder()
+    {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter()
+    {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException
+    {
+        //LOGIC
+        HttpServletRequest request =
+                RequestContext.getCurrentContext().getRequest();
+        logger.info("request -> {} request uri -> {}",
+                request, request.getRequestURI());
+
+        return null;
+    }
+}
