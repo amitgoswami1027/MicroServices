024fb62f0c57d9a97f834a2fca0e647cfb7c231f
Amit Goswami <amitgoswami1027@gmail.com>
2019-09-29 02:11:02 +0530

    echange server

--- currency-exchange-service/src/main/java/com/amitgoswami/micorservices/CurrencyExchangeServiceApplication.java
+++ currency-exchange-service/src/main/java/com/amitgoswami/micorservices/CurrencyExchangeServiceApplication.java
@@ -1,8 +1,10 @@
 package com.amitgoswami.micorservices;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +14,10 @@ public class CurrencyExchangeServiceApplication {
 		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
 	}
 
+	//Added Spring Cloud Sleuth to Zuul Api Gateway Server
+	@Bean
+	public Sampler defaultSampler(){
+
+		return Sampler.ALWAYS_SAMPLE;
+	}
 }
